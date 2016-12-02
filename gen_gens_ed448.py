2cc77122d31dd29bfa606bae68be2bbac545abdb
Sofia Celi <sceli@thoughtworks.com>
2016-12-02 15:24:25 -0500

    Changes name variables to be consistent with protocol

--- gen_gens_ed448.py
+++ gen_gens_ed448.py
@@ -4,8 +4,8 @@ import binascii
 
 b = 448
 d = -39081
-q = 2**448 - 2**224 - 1
-l = 2**446 - 13818066809895115352007386748515426880336692474882178609894547503885
+p = 2**448 - 2**224 - 1
+q = 2**446 - 13818066809895115352007386748515426880336692474882178609894547503885
 cofactor = 4
 
 def H(m):
@@ -19,12 +19,12 @@ def expmod(b,e,m):
 
 # Modular multiplicative inverse
 def inv(x):
-  return expmod(x,q-2,q)
+  return expmod(x,p-2,p)
 
 def xrecover(y):
   xx = (1-y*y) * inv(1-d*y*y)
-  x = expmod(xx, (q+1)/4, q) # x is now the candidate square root of x^2
-  if (x*x - xx) % q != 0: raise Exception("no square root")
+  x = expmod(xx, (p+1)/4, p) # x is now the candidate square root of x^2
+  if (x*x - xx) % p != 0: raise Exception("no square root found")
   return x
 
 # point addition
@@ -35,7 +35,7 @@ def edwards(P,Q):
   y2 = Q[1]
   x3 = (x1*y2+x2*y1) * inv(1+d*x1*x2*y1*y2)
   y3 = (y1*y2-x1*x2) * inv(1-d*x1*x2*y1*y2)
-  return [x3 % q, y3 % q]
+  return [x3 % p, y3 % p]
 
 def scalarmult(P,e):
   if e == 0: return [0,1]
@@ -51,7 +51,7 @@ def bit(h,i):
 def isoncurve(P):
   x = P[0]
   y = P[1]
-  return (x*x + y*y - 1 - d*x*x*y*y) % q == 0
+  return (x*x + y*y - 1 - d*x*x*y*y) % p == 0
 
 # similar to decodeLittleEndian from X25519, but works on bits rather than bytes
 def decodeint(s):
@@ -60,7 +60,7 @@ def decodeint(s):
 def decodepoint(s):
   y = sum(2**i * bit(s,i) for i in range(0,b-1))
   x = xrecover(y)
-  if x & 1 != bit(s,b-1): x = q-x # select the right square root x
+  if x & 1 != bit(s,b-1): x = p-x # select the right square root x
   P = [x,y]
   if not isoncurve(P): raise Exception("decoding point that is not on curve")
   return P
@@ -73,9 +73,9 @@ def find_g(x):
     while True:
         ss = "%s%d" % (x, c)
         try:
-            p = decodepoint(H(ss))
-            g = scalarmult(p, cofactor)
-            is_id = scalarmult(g, l)
+            point = decodepoint(H(ss))
+            g = scalarmult(point, cofactor)
+            is_id = scalarmult(g, q)
             if is_id == identity_element: # IF P^cofactor^primeOrder == [0, 1]
                 return g, ss
         except Exception as e:
